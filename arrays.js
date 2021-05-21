61a6bfdfc5327522f3251e032c41e4c3908ff219
sulbel <20748700+sulbel@users.noreply.github.com>
2021-05-21 10:45:32 -0500

    added common array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,31 @@
 // arrays are declared with square brackets and can have mixed data types
-myArray = ["string", 15];
+var myArray = ["string", 15];
 
+// can have nested arrays
+var myArray = [["array 1", 1], ["array 2", 2]];
+
+// access array data with zero-based indices
+var myArray = [2, 4, 6, 8];
+var myData = myArray[0];
+console.log(myData);
+
+// update data in array
+myArray[0] = 1;
+
+// append data to an array with push()
+myArray.push(10);
+
+// remove data from the end of array with pop()
+console.log("myArray before pop() " + myArray);
+myData = myArray.pop();
+console.log(myData);
+console.log(myArray);
+
+// remove data from the beginning of array with shift()
+console.log("myArray before shift() " + myArray);
+myData = myArray.shift();
+console.log(myData);
+console.log(myArray);
+
+// add data to beginning of array with unshift
+myArray.unshift(2);
