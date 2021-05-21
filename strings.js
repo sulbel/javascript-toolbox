012f430154c27fc5dd6cba043558642a3f6437d9
sulbel <20748700+sulbel@users.noreply.github.com>
2021-05-21 09:20:59 -0500

    added concatenation example

---
+++ strings.js
@@ -0,0 +1,10 @@
+// find the length of a string
+var myName = "StarBoy";
+console.log(myName.length)
+
+// indexing strings
+var firstChar = myName[0];
+console.log(firstChar)
+
+// concatenate strings with + 
+console.log(myName + " " + firstChar)
