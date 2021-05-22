f0e4677071c945ade05633f54b8a884a44a588ea
sulbel <20748700+sulbel@users.noreply.github.com>
2021-05-22 18:19:30 -0500

    added recursive methods for filling arrays with numbers

--- recursion.js
+++ recursion.js
@@ -14,6 +14,37 @@ function sum(arr, n) {
     } else {
       return(sum(arr, n-1) + arr[n-1]);
     }
-  }
+}
 
-  
+// return an array with elements counting from 1->n 
+function countup(n) {
+    if (n < 1) {
+      return [];
+    } else {
+      const countArray = countup(n - 1);
+      countArray.push(n);
+      return countArray;
+    }
+}
+
+// return an array with elements counting n->1
+function countdown(n){
+    if (n<1) {
+      return [];
+    } else {
+      const countArray = countdown(n-1);
+      countArray.unshift(n);
+      return countArray;
+    }
+}
+
+// return an array of numbers counting from [startNum, endNum]
+function rangeOfNumbers(startNum, endNum) {
+    if (startNum === endNum) {
+      return [startNum];
+    } else {
+      const rangeArr = rangeOfNumbers(startNum, endNum-1);
+      rangeArr.push(endNum);
+      return rangeArr;
+    }
+};
